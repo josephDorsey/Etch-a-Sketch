5507fbce463e7eeeb532c76e0be1257ca49fb140
josephDorsey <jmdorsey92@gmail.com>
2023-07-12 10:02:58 -0700

    refactor: remove rowInput and update makeGrid

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const container = document.querySelector(".sketch-container");
 const reset = document.querySelector(".reset");
 const btnAdjustGridSize = document.querySelector(".adjust-grid");
 const adjustInput = document.querySelector(".adjust-input");
-const rowInput = document.querySelector(".row-input");
 for (let i = 0; i < 256; i++) {
   let html = `<div class="box" name="box"></div>`;
   container.insertAdjacentHTML("afterbegin", html);
@@ -27,7 +26,7 @@ reset.addEventListener("click", () => {
 function makeGrid(input) {
   container.style.gridTemplateColumns = `repeat (${input}, 1fr)`;
   container.style.gridTemplateRows = `repeat (${input}, 1fr)`;
-  let grid = row * col;
+  let grid = input * input;
   for (let i = 0; i < grid; i++) {
     let html = `<div class="box" name="box"></div>`;
     container.insertAdjacentHTML("beforeend", html);
@@ -36,5 +35,5 @@ function makeGrid(input) {
 
 btnAdjustGridSize.addEventListener("click", () => {
   container.innerHTML = "";
-  makeGrid(Number(adjustInput.value));
+  makeGrid(adjustInput.value);
 });
